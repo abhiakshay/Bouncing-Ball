4f909297eafb85e11a63c14953c55bc79589af09
Akshay Pathare <abhiakshay@gmail.com>
2017-12-25 20:09:10 +0200

    added save restre and mouse events to canvas

--- src/js/index.js
+++ src/js/index.js
@@ -15,8 +15,11 @@ class BouncingBall {
     }
 
     init() {
+        this.addEvent();
         this.setBackground();
-        this.drawCircle();
+        this.drawBall();
+        this.continue = true;
+        this.centerX = this.canvas.width / 2;
         this.centerY = this.canvas.height / 2;
         this.amplitude = this.centerY - mCircle.r;
         setTimeout(function() {
@@ -25,42 +28,62 @@ class BouncingBall {
         }, 1000);
     }
 
+    addEvent() {
+        this.canvas.addEventListener('mousedown', this.onMouseDown, false);
+        this.canvas.addEventListener('mousemove', this.onMouseMove, false);
+    }
+
+    onMouseDown(evt) {
+        that.continue = !that.continue;
+        if (that.continue) that.animate(that.startTime);
+    }
+
+    onMouseMove(evt) {
+        mCircle.x = evt.clientX;
+    }
+
     setBackground() {
         this.gradient = this.stage.createLinearGradient(0, 0, this.canvas.width, this.canvas.height);
-        this.gradient.addColorStop(0, 'skyblue');
-        this.gradient.addColorStop(1, 'white');
+        this.gradient.addColorStop(0, 'white');
+        this.gradient.addColorStop(0.7, 'skyblue');
+        this.gradient.addColorStop(1, 'green');
         this.stage.fillStyle = this.gradient;
         this.stage.fillRect(0, 0, this.canvas.width, this.canvas.height);
         this.stage.save();
     }
 
-    drawCircle() {
-        this.stage.fillStyle = this.gradient;
-        this.stage.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        this.stage.restore();
+    drawBall() {
         this.stage.moveTo(mCircle.x, mCircle.y);
         this.stage.beginPath();
         this.stage.arc(mCircle.x, mCircle.y, mCircle.r, 0, Math.PI * 2, true);
-        this.stage.fillStyle = 'blue';
+        this.stage.closePath();
+        this.stage.fillStyle = '#FA6900';
         this.stage.fill();
     }
 
+    animateBall() {
+        this.stage.restore();
+        this.stage.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.stage.save();
+        this.drawBall();
+    }
+
     animate(startTime) {
+        if (!that.continue) return;
         // update
-        let time = (new Date()).getTime() - startTime;
+        let time = (new Date()).getTime() - that.startTime || startTime;
         let nextY = that.amplitude * Math.sin(time * 2 * Math.PI / period) + that.centerY;
         mCircle.y = nextY;
-        that.drawCircle();
+        that.animateBall();
+        that.startTime = startTime;
         // request new frame
-        requestAnimFrame(function() {
-            that.animate(startTime);
-        });
+        requestAnimFrame(() => that.animate(startTime));
     }
 
 }
 
 
-window.requestAnimFrame = (function(callback) {
+window.requestAnimFrame = ((callback) => {
     return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
         function(callback) {
             window.setTimeout(callback, 1000 / 60);
